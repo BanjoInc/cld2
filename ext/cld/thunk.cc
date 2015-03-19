ad9afc027a45d84a3c4fa25dd3aa591e38bc6064
Craig Day <cday@zendesk.com>
2015-03-18 21:30:56 -0700

    wrap DetectLanguageSummary

--- ext/cld/thunk.cc
+++ ext/cld/thunk.cc
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "internal/lang_script.h"
@@ -11,6 +12,14 @@ typedef struct {
   const char *code;
   bool reliable;
 } RESULT;
+
+struct summary {
+  const char *name;
+  const char *code;
+  int percent;
+};
+
+typedef struct summary* Summary;
 
 extern "C" {
   RESULT detectLanguageThunkInt(const char * src, bool is_plain_text) {
@@ -37,6 +46,45 @@ extern "C" {
                           normalized_score3,
                           &text_bytes,
                           &is_reliable);
+
+    RESULT res;
+    res.name = LanguageName(lang);
+    res.code = LanguageCode(lang);
+    res.reliable = is_reliable;
+    return res;
+  }
+
+  RESULT detectLanguageSummaryThunkInt(const char * src, bool is_plain_text, Summary * summary3) {
+    bool is_reliable;
+    Language plus_one = UNKNOWN_LANGUAGE;
+    const char* tld_hint = NULL;
+    int encoding_hint = UNKNOWN_ENCODING;
+    Language language_hint = UNKNOWN_LANGUAGE;
+
+    double normalized_score3[3];
+    Language language3[3];
+    int percent3[3];
+    int text_bytes;
+
+    Language lang;
+    lang = ExtDetectLanguageSummary(src,
+                          strlen(src),
+                          is_plain_text,
+                          tld_hint,
+                          encoding_hint,
+                          language_hint,
+                          language3,
+                          percent3,
+                          normalized_score3,
+                          &text_bytes,
+                          &is_reliable);
+
+    for(int i = 0; i < 3; i++) {
+      summary3[i] = (Summary)malloc(sizeof(Summary));
+      summary3[i]->name = LanguageName(language3[i]);
+      summary3[i]->code = LanguageCode(language3[i]);
+      summary3[i]->percent = percent3[i];
+    }
 
     RESULT res;
     res.name = LanguageName(lang);
